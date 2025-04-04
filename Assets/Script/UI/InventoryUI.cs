047b2f3e263cd4551af894f126e89d81c9751634
asoren <zaq55123ooo@gmail.com>
2025-04-04 17:24:19 +0800

    製作物品詳情UI

--- Assets/Script/UI/InventoryUI.cs
+++ Assets/Script/UI/InventoryUI.cs
@@ -2,6 +2,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
+
 namespace Mfram.Inventory
 
 {
---
+++ Assets/Script/UI/ItemToolTip.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMpro;
+using UnityEngine.UI;
+
+
+
+public class ItemToolTip : MonoBehaviour
+{
+    
+}
